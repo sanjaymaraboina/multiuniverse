4ac7fa324d5874ce2367ee026ab266679c502304
sanjaymaraboina <maraboinasanjay.carrerpedia@gmail.com>
2024-05-14 08:40:23 +0530

    commits

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/multiverse" element={<Home />} />
+        <Route path="/multiuniverse" element={<Home />} />
         <Route path="/multiverse/blackhole" element={<BlackHoles />} />
         <Route path="/multiverse/darkMatter" element={<DarkMatter />} />
       </Routes>
